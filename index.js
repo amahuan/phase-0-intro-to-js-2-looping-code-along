39122f9fc241fc1950fcb34a0fd9fb149f9b34e6
Amanda Huang <aamandahuang@gmail.com>
2022-01-22 18:36:13 -0500

    commit tree 04a58f1aac03314c36a6ab63df248d10ac8645b8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names,occassion){
+    const newArray=[];
+    for (let i=0;i<names.length;i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${occassion} gift!`);
+        debugger;
+}
+return newArray;
+}
+writeCards(names);
+
+function countDown(number){
+    for (let i=number; i>=0;i--){
+        console.log(i);
+    }
+}
+countDown(10);
+
